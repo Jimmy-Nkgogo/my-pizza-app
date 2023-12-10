aa5ab75f186e34b41180f9991287f0c2692d06ba
Jimmy <nkgogojimmy@gmail.com>
2023-12-10 23:47:41 +0200

    Started with Register2

--- screens/RegisterScreens/Register1.jsx
+++ screens/RegisterScreens/Register1.jsx
@@ -1,167 +1,203 @@
 import {
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
-    TextInput,
-    Button,
-    KeyboardAvoidingView,
-    ScrollView,
-  } from "react-native";
-  import React, { useState } from "react";
-  
-  const Register1 = ({ navigation }) => {
-    const [userDetails, setUserDetails] = useState([
-      {
-        id: 1,
-        firstName: "",
-        lastName: "",
-        dateOfBirth: "",
-        nationality: "South Africa",
-        mobile: null,
-        email: "",
-      },
-    ]);
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [dateOfBirth, setDateOfBirth] = useState("");
-    const [mobile, setMobile] = useState("");
-    const [email, setEmail] = useState("");
-    const [date, setDate] = useState(new Date());
-    const [open, setOpen] = useState(false);
-  
-    return (
-      <ScrollView style={{ backgroundColor: "white", flex: 1 }}>
-        <KeyboardAvoidingView style={styles.container}>
-          <View>
-            <TouchableOpacity
-              style={styles.btnLogin}
-              onPress={() => navigation.navigate("Login")}
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
+
+const Register1 = ({ navigation }) => {
+  const [userDetails, setUserDetails] = useState([
+    {
+      id: 1,
+      firstName: "",
+      lastName: "",
+      dateOfBirth: "",
+      nationality: "South Africa",
+      mobile: null,
+      email: "",
+    },
+  ]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [email, setEmail] = useState("");
+  const [nationality] = useState("South Africa");
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
+
+  return (
+    <KeyboardAvoidingView style={styles.container}>
+      <View>
+        <TouchableOpacity
+          style={styles.btnLogin}
+          onPress={() => navigation.navigate("Login")}
+        >
+          <Text style={{ color: "white" }}>LOGIN INSTEAD</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.numbers}>
+        <View style={styles.activeNumber}>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 18,
+              textAlign: "center",
+              color: "white",
+            }}
+          >
+            1
+          </Text>
+        </View>
+        <View style={styles.inActiveNumber}>
+          <Text
+            style={{ fontWeight: "bold", fontSize: 18, textAlign: "center" }}
+          >
+            2
+          </Text>
+        </View>
+        <View style={styles.inActiveNumber}>
+          <Text
+            style={{ fontWeight: "bold", fontSize: 18, textAlign: "center" }}
+          >
+            3
+          </Text>
+        </View>
+      </View>
+      {/* Inputs are here */}
+      <ScrollView style={{ width: "100%", flex: 1, marginVertical: 10 }}>
+        <View style={styles.main}>
+          <Text style={styles.headerText}>LET'S GET TO KNOW YOU!</Text>
+          <Text>Enter your Personal Details below.</Text>
+          <View style={{ marginVertical: 15 }}></View>
+
+          <TextInput
+            style={styles.inputStyle}
+            required
+            placeholder="First Name"
+            type="text"
+            value={firstName}
+          />
+          <TextInput
+            style={styles.inputStyle}
+            required
+            placeholder="Last Name"
+            type="text"
+            value={lastName}
+          />
+          <TextInput
+            style={styles.inputStyle}
+            placeholder="Date of Birth (Optional)"
+            value={dateOfBirth}
+          />
+          <View style={{ width: "100%", marginLeft: 38 }}>
+            <Text>NATIONALITY</Text>
+            <TextInput
+              style={styles.inputStyle}
+              placeholder={nationality}
+              value={nationality}
+              editable={false}
+            />
+          </View>
+          <TextInput
+            style={styles.inputStyle}
+            placeholder="Mobile number"
+            type="number"
+            keyboardType="numeric"
+            value={mobile}
+          />
+          <TextInput
+            style={styles.inputStyle}
+            placeholder="Email Address"
+            type="email"
+            value={email}
+          />
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Register2")}
+            style={styles.btnNext}
+          >
+            <Text
+              style={{
+                color: "white",
+                fontWeight: "bold",
+                fontSize: 18,
+                textAlign: "center",
+              }}
             >
-              <Text style={{ color: "white" }}>LOGIN INSTEAD</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.numbers}>
-            <View style={styles.activeNumber}>
-              <Text
-                style={{
-                  fontWeight: "bold",
-                  fontSize: 18,
-                  textAlign: "center",
-                  color: "white",
-                }}
-              >
-                1
-              </Text>
-            </View>
-            <View style={styles.inActiveNumber}>
-              <Text
-                style={{ fontWeight: "bold", fontSize: 18, textAlign: "center" }}
-              >
-                2
-              </Text>
-            </View>
-            <View style={styles.inActiveNumber}>
-              <Text
-                style={{ fontWeight: "bold", fontSize: 18, textAlign: "center" }}
-              >
-                3
-              </Text>
-            </View>
-          </View>
-          {/* Rest of the Screen */}
-          <View style={styles.main}>
-            <Text style={styles.headerText}>LET'S GET TO KNOW YOU!</Text>
-            <Text>Enter your Personal Details below.</Text>
-            <View style={{ marginVertical: 15 }}></View>
-  
-            <TextInput
-              style={styles.inputStyle}
-              required
-              placeholder="First Name"
-              type="text"
-              value={firstName}
-            />
-            <TextInput
-              style={styles.inputStyle}
-              required
-              placeholder="Last Name"
-              type="text"
-              value={lastName}
-            />
-            <TextInput
-              style={styles.inputStyle}
-              placeholder="Date of Birth (Optional)"
-            />
-            <TextInput
-              style={styles.inputStyle}
-              placeholder="Mobile number"
-              type="number"
-              keyboardType="numeric"
-            />
-            <TextInput
-              style={styles.inputStyle}
-              placeholder="Email Address"
-              type="email"
-            />
-          </View>
-        </KeyboardAvoidingView>
+              Next
+            </Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
-    );
-  };
-  
-  export default Register1;
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      paddingVertical: 15,
-      backgroundColor: "white",
-    },
-    btnLogin: {
-      backgroundColor: "skyblue",
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderRadius: 30,
-    },
-    numbers: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      width: "90%",
-      marginVertical: 20,
-    },
-    activeNumber: {
-      backgroundColor: "#333",
-      borderRadius: 20,
-      width: 25,
-    },
-    inActiveNumber: {
-      borderColor: "#333",
-      borderRadius: 20,
-      width: 25,
-      borderWidth: 1,
-    },
-    headerText: {
-      fontSize: 30,
-      fontWeight: "900",
-      width: 200,
-      textAlign: "center",
-    },
-    main: {
-      flex: 1,
-      alignItems: "center",
-      width: "100%",
-      backgroundColor: "white",
-    },
-    inputStyle: {
-      width: "90%",
-      height: 48,
-      marginVertical: 10,
-      backgroundColor: "#eee",
-      borderRadius: 10,
-      paddingHorizontal: 10,
-    },
-  });
-  
+    </KeyboardAvoidingView>
+  );
+};
+
+export default Register1;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 15,
+    backgroundColor: "white",
+    marginBottom: 0,
+  },
+  btnLogin: {
+    backgroundColor: "skyblue",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+  },
+  numbers: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "90%",
+    marginVertical: 20,
+  },
+  activeNumber: {
+    backgroundColor: "#333",
+    borderRadius: 20,
+    width: 25,
+  },
+  inActiveNumber: {
+    borderColor: "#333",
+    borderRadius: 20,
+    width: 25,
+    borderWidth: 1,
+  },
+  headerText: {
+    fontSize: 30,
+    fontWeight: "900",
+    width: 200,
+    textAlign: "center",
+  },
+  main: {
+    flex: 1,
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "white",
+  },
+  inputStyle: {
+    width: "90%",
+    height: 48,
+    marginVertical: 10,
+    backgroundColor: "#eee",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+  },
+  btnNext: {
+    backgroundColor: "blue",
+    width: "90%",
+    height: 48,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 18,
+    marginVertical: 20,
+  },
+});
--- screens/RegisterScreens/Register2.jsx
+++ screens/RegisterScreens/Register2.jsx
@@ -1,14 +1,160 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
 
-const Register2 = () => {
+const Register2 = ({ navigation }) => {
+  const [dietaryReq, setDietaryReq] = useState([
+    {
+      id: 1,
+      req: "",
+    },
+  ]);
+  const [req, setReq] = useState("");
+
   return (
-    <View>
-      <Text>Register2</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container}>
+      <View>
+        <TouchableOpacity
+          style={styles.btnLogin}
+          onPress={() => navigation.navigate("Login")}
+        >
+          <Text style={{ color: "white" }}>LOGIN INSTEAD</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.numbers}>
+        <View style={styles.inActiveNumber}>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 18,
+              textAlign: "center",
+            }}
+          >
+            1
+          </Text>
+        </View>
+        <View style={styles.activeNumber}>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 18,
+              textAlign: "center",
+              color: "white",
+            }}
+          >
+            2
+          </Text>
+        </View>
+        <View style={styles.inActiveNumber}>
+          <Text
+            style={{ fontWeight: "bold", fontSize: 18, textAlign: "center" }}
+          >
+            3
+          </Text>
+        </View>
+      </View>
+      {/* Inputs are here */}
+      <ScrollView style={{ width: "100%", flex: 1, marginVertical: 10 }}>
+        <View style={styles.main}>
+          <Text style={styles.headerText}>MY DIETARY REQUIREMENTS</Text>
+          <Text>Select your dietary requirements below.</Text>
+          <View style={{ marginVertical: 15 }}></View>
+          <TouchableOpacity>
+            <Text>Halaal</Text>
+          </TouchableOpacity>
+
+          <View style={styles.btnViewStyle}>
+
+          </View>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Register2")}
+            style={styles.btnNext}
+          >
+            <Text
+              style={{
+                color: "white",
+                fontWeight: "bold",
+                fontSize: 18,
+                textAlign: "center",
+              }}
+            >
+              Next
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
 export default Register2;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 15,
+    backgroundColor: "white",
+    marginBottom: 0,
+  },
+  btnLogin: {
+    backgroundColor: "skyblue",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+  },
+  numbers: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "90%",
+    marginVertical: 20,
+  },
+  activeNumber: {
+    backgroundColor: "#333",
+    borderRadius: 20,
+    width: 25,
+  },
+  inActiveNumber: {
+    borderColor: "#333",
+    borderRadius: 20,
+    width: 25,
+    borderWidth: 1,
+  },
+  headerText: {
+    fontSize: 28,
+    fontWeight: "900",
+    width: 215,
+    textAlign: "center",
+  },
+  main: {
+    flex: 1,
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "white",
+  },
+  inputStyle: {
+    width: "90%",
+    height: 48,
+    marginVertical: 10,
+    backgroundColor: "#eee",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+  },
+  btnNext: {
+    backgroundColor: "blue",
+    width: "90%",
+    height: 48,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 18,
+    marginVertical: 20,
+  },
+});
