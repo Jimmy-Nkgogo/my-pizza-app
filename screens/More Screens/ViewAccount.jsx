1ff9de1decceb5ab55beed8bbd3649214ee1f04e
Jimmy <nkgogojimmy@gmail.com>
2023-12-23 02:07:52 +0200

    started View Account UI

--- screens/More Screens/ViewAccount.jsx
+++ screens/More Screens/ViewAccount.jsx
@@ -1,15 +1,38 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const ViewAccount = ({ navigation, route }) => {
   const user = route.params.user;
   return (
-    <View>
-      <Text>{user} </Text>
+    <View style={styles.container}>
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+        <Ionicons name="chevron-back-outline" size={28} color="gray" />
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            marginLeft: "25%",
+          }}
+        >
+          <FontAwesome5 name="user-circle" size={48} color="black" />
+          <Text style={{ fontSize: 23, fontWeight: "bold" }}>{user}</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default ViewAccount;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "red",
+    flex: 1,
+  },
+});
