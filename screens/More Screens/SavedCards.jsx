f78cb42919d87417f097d1fb514f51bff78ba7f9
Jimmy <nkgogojimmy@gmail.com>
2023-12-27 11:07:37 +0200

    added the necessary useStates

--- screens/More Screens/SavedCards.jsx
+++ screens/More Screens/SavedCards.jsx
@@ -1,13 +1,29 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import BackButton from "../../components/BackButton";
+import LineBreak from "../../components/LineBreak";
 
 const SavedCards = () => {
+  const [userCard, setUserCard] = useState([
+    {
+      id: 1,
+      cardHolder: "MJ NKGOGO",
+      cardNumber: "4847958248327726",
+      expDate: "10/27",
+      cvv: "123",
+    },
+  ]);
+  const [cardHolder, setCardHolder] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [expDate, setExpDate] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [emptyCard, setEmptyCard] = useState(true);
+
   return (
     <View style={styles.container}>
       <BackButton title="Payment Options" />
-      <View style={{ backgroundColor: "red", flex: 1, width: "95%" }}>
-        <Text>SavedCards</Text>
+      <View style={{ backgroundColor: "whitesmoke", flex: 1, width: "95%" }}>
+        <Text style={styles.text}>Add new card</Text>
       </View>
     </View>
   );
@@ -20,5 +36,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "white",
     alignItems: "center",
+  },
+  text: {
+    color: "#777",
   },
 });
